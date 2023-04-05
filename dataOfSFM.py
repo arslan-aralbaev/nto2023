765e36922f1fe122afd2c81a2c0b8a3435c6f8b0
Alex <arslan.aralbaev@yandex.ru>
2023-04-05 15:08:25 +0500

    Corrected programm dataOfSMF

--- dataOfSFM.py
+++ dataOfSFM.py
@@ -1,14 +1,16 @@
-konst = {'a': 0,
-		 'b': 2,
-		 'c': 1}
+konst = {'фиалетовое': '0',
+		 'прозрачное': '4',
+		 'красное': '6'}
 
 
-typeOfSMF = input()
-diameter = int(input())
+typeOfSMF = input().lower()
+diameter = input('Этот параметр вам ненужон, нажмите ENTER')
 
 
 def result(type, dia):
-	return konst[typeOfSMF] * diameter
+	if type == 'фиалетовое':
+		return 'У данного оптоволокна критический радиус очень мал для измерения'
+	return konst[typeOfSMF] + 'mm' if type in konst else 'Нет данного типа волокна'
 
 
 print(result(typeOfSMF, diameter))
