470500ba718e847fe4529bcce3804025340cf03b
Alex <arslan.aralbaev@yandex.ru>
2023-04-05 14:35:30 +0500

    data base, calculations of SMF

---
+++ dataOfSFM.py
@@ -0,0 +1,12 @@
+konst = {'a': 0,
+		 'b': 2,
+		 'c': 1}
+typeOfSMF = input()
+diameter = int(input())
+
+
+def result(type, dia):
+	return konst[typeOfSMF] * diameter
+
+
+print(result(typeOfSMF, diameter))
